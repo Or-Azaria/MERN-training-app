d3d12b93d7046df14a86f6e3d6956d2be6622753
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-12-03 16:33:23 +0200

    adding after removing

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-require('dotenv').config();
-
 const express = require('express');
-const workoutsRoutes = require('./routes/workouts');
 const mongoose = require('mongoose');
+const workoutsRoutes = require('./routes/workouts');
 //express app
 const app = express();
 
