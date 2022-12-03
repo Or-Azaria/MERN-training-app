66b660be7ae25bb3440b510828dad6e70e7b3fd8
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-12-04 01:10:48 +0200

    add for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="pages"></div>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
