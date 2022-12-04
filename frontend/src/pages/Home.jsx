f35003f88b9c887aee4e972a3dbe7a308facd184
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-12-04 23:51:54 +0200

    add index.css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="Home">
+    <div className="home">
       <div className="workouts">
         {workouts &&
           workouts.map((workout) => (
