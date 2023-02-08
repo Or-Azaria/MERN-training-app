f3cdc0c13aed3a40b2e215933a19ffd6cf1eaa9d
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-02-08 21:22:48 +0200

    add

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+const user = require('../models/userModel');    
+
 // login user
 const loginUser = async (req, res) => {
   res.json({ mssg: 'login user' });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-  }
-);
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Workout', userSchema);
+module.exports = mongoose.model('User', userSchema);
