388b06c7fd78237e7d6c735d0f9fc818d7ecae19
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-03-18 15:24:52 +0200

    add

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ export const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button>Login</button>
+      <button>Log in</button>
     </form>
   );
 };
