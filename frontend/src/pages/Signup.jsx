910997dcfbb6227ca529c6355514a1d998815bfa
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-03-24 19:10:22 +0300

    new

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -27,7 +27,7 @@ const Signup = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button disabled={isLoading}>Sig up</button>
+      <button disabled={isLoading}>Sign up</button>
       {error && <div className="error">{error}</div>}
     </form>
   );
