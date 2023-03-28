86c2314de33a107efdbc9612424f65e14de1d3fd
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-03-28 21:46:52 +0300

    new

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -20,7 +20,7 @@ export default function NavBar() {
           </div>
           <div>
             <Link to="/login">Login</Link>
-            <Link to="/signup">Sinup</Link>
+            <Link to="/signup">Signup</Link>
           </div>
         </nav>
       </div>
