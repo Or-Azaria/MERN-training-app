c22f5105b90fab38d6d4f46d8c9230946dc18bbd
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-05-06 19:08:28 +0300

    backend middleware

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+  // verify authentication
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'Authorization token required' });
+  }
+  const token = authorization.split(' ')[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: 'Request is not authorized' });
+  }
+};
