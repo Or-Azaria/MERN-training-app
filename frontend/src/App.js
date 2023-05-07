a4e67a039244bc1fbce0cb697465dfb8ac8baa9e
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-05-07 17:53:41 +0300

    add navigate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigateß } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
 
 // pages & components
@@ -8,15 +8,26 @@ import Signup from './pages/Signup';
 import NavBar from './components/NavBar';
 
 export default function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
