95f0beb0ff1180c3e4a0ff835838371a9ddfebcd
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-05-27 14:07:21 +0300

    update

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -19,7 +19,7 @@ export default function NavBar() {
           {user && (
             <div>
               <span>{user.email}</span>
-              <button onClick={handleClick}>Log ut</button>
+              <button onClick={handleClick}>Log out</button>
             </div>
           )}
           {!user && (
